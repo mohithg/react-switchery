bc521f383a0b0e845532b20b8a1b0dd2bf330b7b
Mohith G <mohithgm@gmail.com>
2017-10-20 13:13:48 +0530

    Fix lint

--- src/index.js
+++ src/index.js
@@ -10,79 +10,79 @@ import PropTypes from 'prop-types';
  */
 class Switch extends React.Component {
 
-    /**
-     * We initialize the Switchery object
-     * once the component is mounted
-     */
-    componentDidMount() {
-        const input = this.elCheckbox;
+  /**
+   * We initialize the Switchery object
+   * once the component is mounted
+   */
+  componentDidMount() {
+    const input = this.elCheckbox;
 
-        /* eslint-disable no-undef, no-new */
-        new Switchery(input, this.props.options);
-        /* eslint-enable no-new, no-undef */
-        input.onchange = this.onChange;
-    }
+    /* eslint-disable no-undef, no-new */
+    new Switchery(input, this.props.options);
+    /* eslint-enable no-new, no-undef */
+    input.onchange = this.onChange;
+  }
 
-    /**
-     * When the user makes a change
-     * If an external onChange
-     * function is provided, we call that.
-     */
-    onClick = () => { //eslint-disable-line no-undef
-        if (this.props.onChange) {
-            this.props.onChange(this.elCheckbox.checked);
-        }
-        if(this.elCheckbox.checked) {
-            this.elWrapper.className = this.elWrapper.className.split(' ').join(' ') + ' isChecked';
-        } else {
-            this.elWrapper.className = this.elWrapper.className.replace('isChecked', '');
-        }
+  /**
+   * When the user makes a change
+   * If an external onChange
+   * function is provided, we call that.
+   */
+  onClick = () => { // eslint-disable-line
+    if (this.props.onChange) {
+      this.props.onChange(this.elCheckbox.checked);
     }
+    if (this.elCheckbox.checked) {
+      this.elWrapper.className = `${this.elWrapper.className.split(' ').join(' ')} isChecked`;
+    } else {
+      this.elWrapper.className = this.elWrapper.className.replace('isChecked', '');
+    }
+  }
 
-    /**
-     * renders the component
-     */
-    render() {
-        return (
-            <div
-                onClick={this.onClick}
-                className={classNames([
-                    this.props.className,
-                    {
-                        required: this.props.required,
-                    },
-                ])}
-                ref={elWrapper => this.elWrapper = elWrapper}
-            >
-                {(this.props.label)? <label>{this.props.label}</label> : null }
-              <input
-                  ref={elCheckbox => this.elCheckbox = elCheckbox}
-                  type="checkbox"
-                  defaultChecked={this.props.checked}
-              />
-            </div>
-        );
-    }
+  /**
+   * renders the component
+   */
+  render() {
+    return (
+      <div
+        onClick={this.onClick}
+        className={classNames([
+          this.props.className,
+          {
+            required: this.props.required,
+          },
+        ])}
+        ref={elWrapper => { this.elWrapper = elWrapper; }}
+      >
+        {(this.props.label) ? <label>{this.props.label}</label> : null}
+        <input
+          ref={elCheckbox => { this.elCheckbox = elCheckbox; }}
+          type="checkbox"
+          defaultChecked={this.props.checked}
+        />
+      </div>
+    );
+  }
 }
 
 /**
  * Validating propTypes
  */
 Switch.propTypes = {
-    label: PropTypes.string,
-    className: PropTypes.string,
-    required: PropTypes.bool,
-    checked: PropTypes.bool,
-    options: PropTypes.object,
-    onChange: PropTypes.func,
+  label: PropTypes.string,
+  className: PropTypes.string,
+  required: PropTypes.bool,
+  checked: PropTypes.bool,
+  options: PropTypes.object,
+  onChange: PropTypes.func,
 };
 
 /**
  * Default Props
  */
 Switch.defaultProps = {
-    value: true,
-    required: false,
+  value: true,
+  required: false,
 };
 
 /**
